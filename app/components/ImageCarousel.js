379db47c50a6ca0a88ab16ff7f6077896a1a9a24
Jake Peyser <jakepeyser@gmail.com>
2017-01-15 23:06:34 -0500

    image rotation functional

--- app/components/ImageCarousel.js
+++ app/components/ImageCarousel.js
@@ -5,27 +5,51 @@ class ImageCarousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      curSlide: this.props.initSlide
+      curSlide: this.props.initSlide,
+      carouselRotate: '0%'
     }
+    this.changeSlides = this.changeSlides.bind(this);
+  }
+
+  // Rotate current slide depending on button clicked
+  changeSlides(side) {
+    const newSlide = side === 'prev' ?
+      this.state.curSlide - 1 : this.state.curSlide + 1;
+    const newRotation = `${newSlide * -100}%`;
+    this.setState({
+      curSlide: newSlide,
+      carouselRotate: newRotation
+    })
   }
 
   render() {
     return (
       <div className="image-carousel">
-        <ul className="images">
+        <ul className="images"
+          style={{ marginLeft: this.state.carouselRotate }}>
         { // Nest all images inside list elements
-          this.props.images.map(image => {
+          this.props.images.map((image, idx) => {
+            let slideWidthPct = 100 / this.props.images.length;
             return (
               <li key={ image } className="image"
-                style={{ width: `${100 / this.props.images.length}%` }}>
+                style={{
+                  width: `${slideWidthPct}%`,
+                  left: `${(slideWidthPct * idx)}%`
+                }}>
                 <img src='http://www.mindshiftinteractive.com/wp-content/uploads/2016/10/test-big.png' />
               </li>
             )
           })
         }
         </ul>
-        <ImageSwitcher side="prev"/>
-        <ImageSwitcher side="next"/>
+        { // Only show left switcher when curSlide is nonzero
+          this.state.curSlide ?
+            <ImageSwitcher side="prev" switchFn={ this.changeSlides } /> : null
+        }
+        { // Only show right switcher when not on last slide
+          this.state.curSlide < this.props.images.length - 1 ?
+            <ImageSwitcher side="next" switchFn={ this.changeSlides } /> : null
+        }
       </div>
     )
   }
--- app/components/ImageSwitcher.js
+++ app/components/ImageSwitcher.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 // Button used for navigating the ImageCarousel
 // Input prop 'side' must be either "left" or "right"
-export default ({ side }) => (
+export default ({ side, switchFn }) => (
   <div className={`switcher ${side}`}>
-    <button className={`${side}`} />
+    <button className={`${side}`}
+      onClick={ () => switchFn(side) }
+    />
   </div>
 )
