f5cac2602068eece678d43c1615707fa9bb7d871
Jake Peyser <jakepeyser@gmail.com>
2017-01-15 21:47:30 -0500

    basic images displaying
    tests and css added

--- app/components/ImageCarousel.js
+++ app/components/ImageCarousel.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-export default () => (
-  <div className="image-carousel">
-    Hello World
-  </div>
-)
+class ImageCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curSlide: this.props.initSlide
+    }
+  }
+
+  render() {
+    return (
+      <div className="image-carousel">
+        <ul className="images">
+        { // Nest all images inside list elements
+          this.props.images.map(image => {
+            return (
+              <li key={ image } className="image"
+                style={{ width: `${100 / this.props.images.length}%` }}>
+                <img src='http://www.mindshiftinteractive.com/wp-content/uploads/2016/10/test-big.png' />
+              </li>
+            )
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+}
+
+ImageCarousel.defaultProps = {
+  images: [],
+  initSlide: 0
+}
+
+ImageCarousel.propTypes = {
+  images: React.PropTypes.arrayOf(React.PropTypes.string),
+  initSlide: React.PropTypes.number
+};
+
+export default ImageCarousel;
